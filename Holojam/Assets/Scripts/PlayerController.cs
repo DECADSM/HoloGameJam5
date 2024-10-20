df4cbd9a9a39d78aac28c8baa23d27e6fe88fe67
LJNovitch <ljnovitch@gmail.com>
2024-10-19 21:40:58 -0700

    created a basic PlayerController and set up some of the inputs for the Input System Package

--- Holojam/Assets/Scripts/PlayerController.cs
+++ Holojam/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    private InputAction moveAction;
+    private InputAction jumpAction;
+
+
+    public float jumpStrength = 5f;
+    public float moveSpeed = 50f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        moveAction = InputSystem.actions.FindAction("Move");
+        jumpAction = InputSystem.actions.FindAction("Jump");
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+
+        Vector2 moveValue = moveAction.ReadValue<Vector2>();
+        //print(moveValue);
+        // your movement code here
+        rb.velocity = new Vector2(moveValue.x * moveSpeed * Time.deltaTime, rb.velocity.y);
+        if (jumpAction.IsPressed() && rb.velocity.y == 0)
+        {
+           // print("jump");
+            // your jump code here
+            rb.velocity = new Vector2(rb.velocity.x, jumpStrength);
+        }
     }
+
+    
 }
