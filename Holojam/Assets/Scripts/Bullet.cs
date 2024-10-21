cc83414769df71d65ca78e55fef7b146e3886b1b
LJNovitch <ljnovitch@gmail.com>
2024-10-20 21:00:15 -0700

    player now shoot basic bullets

--- Holojam/Assets/Scripts/Bullet.cs
+++ Holojam/Assets/Scripts/Bullet.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public float lifetime = 3f;
+    public float speed = 100f;
+    public int damage = 1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,11 @@ public class Bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        lifetime -= Time.deltaTime;
+
+        if( lifetime <= 0 )
+        {
+            GameObject.Destroy(gameObject);
+        }
     }
 }
