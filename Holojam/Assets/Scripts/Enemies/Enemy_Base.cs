5a3aa8bc36f684264d2d00230f8e4e0dffb0725f
decadsm <duke129409@gmail.com>
2024-10-21 21:16:15 -0400

    Testing the moving of the enemy along pathfinding

--- Holojam/Assets/Scripts/Enemies/Enemy_Base.cs
+++ Holojam/Assets/Scripts/Enemies/Enemy_Base.cs
@@ -14,9 +14,12 @@ public class Enemy_Base : MonoBehaviour
     public float JumpDistance = 5;
 
     public PlayerController player;
-    bool moving = false;
+    [SerializeField] bool moving = false;
 
     public Vector2[] PatrolPoints;
+    Queue<Node> closedList;
+    Node previous;
+    [SerializeField]bool grounded = true;
 
     // Start is called before the first frame update
     void Start()
@@ -31,11 +34,45 @@ public class Enemy_Base : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.P))
         {
-            pf.PathFinding();
+            if(!CheckLOS())
+                pf.PathFinding();
+            closedList = new Queue<Node>(pf.ClosedList);
+            closedList.Dequeue(); //This gets rid of the first node which is the enemy
         }
-        //if(CheckLOS() && moving == true)
-        if(moving)
-            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, MoveSpeed * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.L))
+            Jump();
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1);
+        if (hit.collider != null)
+        {
+            if (hit.collider.name.Contains("Platform") || hit.collider.name.Contains("Floor"))
+            {
+                grounded = true;
+            }
+        }
+        //if (CheckLOS() && moving == true)
+        if (moving)
+        {
+            if (closedList.Count > 0 && !CheckLOS())
+            {
+                float dist = Vector2.Distance(transform.position, closedList.Peek().transform.position);
+                if (dist > 3)
+                {
+                    
+                    if (closedList.Peek().transform.position.y > transform.position.y || previous.name.Contains("Left"))
+                    { 
+                            Jump();
+                    }
+                    transform.position = Vector2.MoveTowards(transform.position, closedList.Peek().transform.position, MoveSpeed * Time.deltaTime);
+                }
+                else
+                {
+                    previous = closedList.Peek();
+                    closedList.Dequeue();
+                }
+            }
+            else
+                transform.position = Vector2.MoveTowards(transform.position, player.transform.position, MoveSpeed * Time.deltaTime);
+        }
 
     }
     private void OnCollisionEnter2D(Collision2D collision)
@@ -78,6 +115,10 @@ public class Enemy_Base : MonoBehaviour
 
     public virtual void Jump()
     {
-        GetComponent<Rigidbody>().velocity = new Vector2(GetComponent<Rigidbody>().velocity.x, JumpStrength);
+        if (grounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, JumpStrength);
+            grounded = false;
+        }
     }
 }
