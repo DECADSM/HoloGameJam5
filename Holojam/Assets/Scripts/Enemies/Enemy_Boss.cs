88cb9e65c8411e8a40f01b0236b4bfb4bca7a567
decadsm <duke129409@gmail.com>
2024-10-24 22:55:15 -0400

    More Boss setup, moves and timers

--- Holojam/Assets/Scripts/Enemies/Enemy_Boss.cs
+++ Holojam/Assets/Scripts/Enemies/Enemy_Boss.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 
 public class Enemy_Boss : MonoBehaviour
 {
+    [Header("Stats")]
     [SerializeField] float Health;
+    [SerializeField] float SlamDamage;
+    [SerializeField] float ShieldTimer;
+    float ShieldTimerRunning = 0;
+    [SerializeField] float ShotTimer;
+    float ShotTimerRunning = 0;
+    [SerializeField] float NumOfShots;
+    [SerializeField] float EnterCorruptionTimer;
+    float EnterCorruptionTimerRunning = 0;
+    [SerializeField] float CorruptionDuration;
+    float CorruptionDurationRunning = 0;
+    [SerializeField] float KnockbackStrength;
+
+    [Space(5)]
+    [Header("Flags")]
+    [SerializeField] bool Corruption = false;
 
     private SpriteRenderer SR;
     private Color OGColor;
 
     //Player
+    [Space(5)]
     [SerializeField] GameObject Player;
     PlayerController PC;
 
@@ -18,6 +35,9 @@ public class Enemy_Boss : MonoBehaviour
     [SerializeField] GameObject Shield;
     [SerializeField] GameObject Bullet;
     [SerializeField] BoxCollider2D StompCollider;
+    [SerializeField] Transform BulletSpawner;
+
+    int RandomAction = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -31,7 +51,35 @@ public class Enemy_Boss : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (ShieldTimerRunning > 0)
+            ShieldTimerRunning -= Time.deltaTime;
+
+        if (CorruptionDurationRunning > 0)
+            CorruptionDurationRunning -= Time.deltaTime;
+
+        if (ShotTimerRunning > 0)
+            ShotTimerRunning -= Time.deltaTime;
+
+        if (EnterCorruptionTimerRunning > 0)
+            EnterCorruptionTimerRunning -= Time.deltaTime;
+
+        RandomAction = Random.Range(1, 5);
+
+        switch(RandomAction)
+        {
+            case 1://Shoot
+                break;
+            case 2://Rush
+                break;
+            case 3://Stomp
+                break;
+            case 4://Shield
+                break;
+            case 5://Corruption State
+                break;
+            default:
+                break;
+        }
     }
 
     public void TakeDamage(float dmg)
@@ -49,19 +97,40 @@ public class Enemy_Boss : MonoBehaviour
 
 
     //Move Set
-    private void DeployShield()
+    private void ShieldSwitch()
     {
-
+        if (Shield.activeSelf)
+            Shield.SetActive(false);
+        else
+            Shield.SetActive(true);
     }
 
     void ShotgunShot()
     {
+        List<GameObject> ShotgunPellets = new List<GameObject>();
+        Vector2 direction = (Player.transform.position - BulletSpawner.position).normalized;
 
+        for (int i = 0; i < NumOfShots; i++)
+        {
+            ShotgunPellets.Add(Instantiate(Bullet, (direction * 1.5f) + new Vector2(transform.position.x, transform.position.y + Random.Range(-2,2)), new Quaternion()));
+        }
     }
 
     void Stomp()
     {
+        //Jump up and slam down in a part of the arena, flipping to the player
+    }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            PC.RemoveHealth(SlamDamage);
+            PC.GettingHit();
+
+            Vector2 movePlayerUp = new Vector2(Player.transform.position.x, Player.transform.position.y + 2);
+            Player.GetComponent<Rigidbody2D>().AddForce((movePlayerUp - new Vector2(transform.position.x, transform.position.y)).normalized * KnockbackStrength, ForceMode2D.Impulse);
+        }
     }
 
     void Rush()
